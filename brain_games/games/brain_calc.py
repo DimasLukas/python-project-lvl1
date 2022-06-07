12fa172337250105b2d7ee156e67097f7d97a847
Dimaslukas <lukin_dima@mail.ru>
2022-06-07 18:55:03 +0300

    correction of brain_calc

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,18 +1,21 @@
 import prompt
 import random
+from operator import add, mul, sub
 Uniq_game_message = 'What is the result of the expression?'
+
+operators = {
+    '+': add,
+    '-': sub,
+    '*': mul
+}
+keys = list(operators)
 
 
 def question_for_user():
     first_number = random.randint(0, 100)
     second_number = random.randint(0, 100)
-    symbol = random.choice(['+', '-', '*'])
-    if symbol == '+':
-        result = first_number + second_number
-    if symbol == '-':
-        result = first_number - second_number
-    if symbol == '*':
-        result = first_number * second_number
-    print("Question: ", first_number, symbol, second_number)
+    picked_operator = random.choice(keys)
+    print("Question: ", first_number, picked_operator, second_number)
+    result = (operators[picked_operator](first_number, second_number))
     user_answer = prompt.integer('Your answer: ')
     return(result, user_answer)
