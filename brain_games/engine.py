3a0d1c8b69d805059045bf586fb08c1158880d9c
Dimaslukas <lukin_dima@mail.ru>
2022-06-20 21:43:48 +0300

    correction of engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,4 +1,5 @@
 import prompt
+NUMBER_OF_ROUNDS = 3
 
 
 def play_game(game):
@@ -7,9 +8,7 @@ def play_game(game):
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game.GAME_RULES)
-    NUMBER_OF_ROUNDS = 3
-    rounds = 1
-    for rounds in range(NUMBER_OF_ROUNDS):
+    for i in range(NUMBER_OF_ROUNDS):
         question, right_answer = \
             game.generate_question_and_right_answer()
         print(f"Question: {question}")
@@ -17,10 +16,11 @@ def play_game(game):
 
         if user_answer == right_answer:
             print('Correct!')
-            rounds += 1
+            i += 1
         else:
             print(f"{user_answer} is wrong answer; "
                   f"(. Correct answer was {right_answer}.")
             print(f"Let's try again, {name}!")
-            return
-    print(f'Congratulations, {name}!')
+            break
+    else:
+        print(f'Congratulations, {name}!')
